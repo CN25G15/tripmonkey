037590597f6ee0ec86c48733e35e8212b2874426
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-04-15 17:24:55 +0100

    Deployment feature 1

---
+++ build-images.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+
+ECHO "  ______ ______ ______ ______ ______ ______ ______ ______ ______ ______  " 
+ECHO " |______|______|______|______|______|______|______|______|______|______| "
+ECHO "       |__   __| (_)                           | |                       "
+ECHO "          | |_ __ _ _ __  _ __ ___   ___  _ __ | | _____ _   _           "
+ECHO "          | | '__| | '_ \| '_ ` _ \ / _ \| '_ \| |/ / _ | | | |          "
+ECHO "          | | |  | | |_) | | | | | | (_) | | | |   |  __| |_| |          "
+ECHO "          |_|_|  |_| .__/|_| |_| |_|\___/|_| |_|_|\_\___|\__, |          "
+ECHO "                   | |                                    __/ |          "
+ECHO "  ______ ______ ___|_| ______ ______ ______ ______ ______|___/__ ______  "
+ECHO " |______|______|______|______|______|______|______|______|______|______| "
+
+
+ECHO.
+ECHO ---------------------------------------
+ECHO Building images for local docker repo
+ECHO ---------------------------------------
+ECHO.
+
+FOR /D %%D IN (*) DO (
+
+    IF EXIST "%%~D\Dockerfile" (
+        echo found Dockerfile @ %%~D
+        docker build -t "tripmonkey/%%~D" "%%~D"
+    ) ELSE IF EXIST "%%~D\build-image.bat" (
+        echo found build-image.bat @ %%~D
+        CALL %cd%\%%~D\build-image.bat
+    ) ELSE (
+        ECHO No Dockerfile or build-image.sh found @ %%~D
+    )
+)
+
+EXIT /B 0
---
+++ build-images.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "  ______ ______ ______ ______ ______ ______ ______ ______ ______ ______ 
+ |______|______|______|______|______|______|______|______|______|______|
+       |__   __| (_)                           | |                      
+          | |_ __ _ _ __  _ __ ___   ___  _ __ | | _____ _   _          
+          | | '__| | '_ \| '_ ` _ \ / _ \| '_ \| |/ / _ | | | |         
+          | | |  | | |_) | | | | | | (_) | | | |   |  __| |_| |         
+          |_|_|  |_| .__/|_| |_| |_|\___/|_| |_|_|\_\___|\__, |         
+                   | |                                    __/ |         
+  ______ ______ ___|_| ______ ______ ______ ______ ______|___/__ ______ 
+ |______|______|______|______|______|______|______|______|______|______|
+                                                                        "
+
+echo "\n-------------------------------------\nBuilding images for local docker repo\n-------------------------------------\n"
+
+# Loop over each subdirectory in the current directory
+for dir in */; do
+  # Check if a Dockerfile exists in the current directory
+  if [ -f "${dir}Dockerfile" ]; then
+    # If it does, use docker build to build the image
+    echo "found Dockerfile @ ${dir}"
+	docker build -t "tripmonkey/${dir}" "${dir}"
+	
+  else
+    # If there's no Dockerfile, look for a script with the name "build-image.sh"
+    if [ -x "${dir}build-image.sh" ]; then
+      # If it exists and is executable, run the script
+	  echo "found build-image.sh @ ${dir}"
+	  bash "${dir}build-image.sh"
+    else
+      # If neither a Dockerfile nor "build-image.sh" exist in this directory, skip it
+      echo "No Dockerfile or build-image.sh found @ ${dir}"
+    fi
+  fi
+done
