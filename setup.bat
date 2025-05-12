d42e1bcf5f199972e07b745bd240dfb5e43ba50e
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-05-12 11:59:47 +0100

    Functional local development environment directly integrated with kubernetes via skaffold

--- setup.bat
+++ setup.bat
@@ -12,19 +12,28 @@ ECHO "  ______ ______ ___|_| ______ ______ ______ ______ ______|___/__ ______  "
 ECHO " |______|______|______|______|______|______|______|______|______|______| "
 
 IF /I "%~1"=="dev" (
-    SET env=dev
+    SET env=tripmonkey-dev
+    SET target=0.development
 ) ELSE IF /I "%~1"=="staging" (
-    SET env=stage
+    SET env=tripmonkey-stage
+    SET target=1.staging
 ) ELSE IF /I "%~1"=="prod" (
-    SET env=prod
+    SET env=tripmonkey-prod
+    SET target=2.production
 ) ELSE (
-    SET env=dev
+    SET env=tripmonkey-dev
+    SET target=0.development
 )
 
 
+minikube start --profile tripmonkey
 minikube addons enable metrics-server
 minikube addons enable ingress
-minikube start
-CALL deploy-configs.bat %env%
-CALL deploy-secrets.bat %env%
-CALL install-dependencies.bat %env%
+ECHO "Installing dependencies (Kafka and Mongo Operators) for environment: %env%"
+kubectl apply -f https://strimzi.io/install/latest?namespace=default -n default
+kubectl apply -f build/mongodb-kubernetes-operator/config/crd/bases/mongodbcommunity.mongodb.com_mongodbcommunity.yaml
+kubectl apply -f build/mongodb-kubernetes-operator/deploy/clusterwide
+kubectl apply -k build/mongodb-kubernetes-operator/config/rbac/ --namespace default
+kubectl create -f build/mongodb-kubernetes-operator/config/manager/manager.yaml --namespace default
+kubectl apply -f k8s/environments/%target%/pre-deploy
+
--- setup.sh
+++ setup.sh
@@ -12,25 +12,32 @@ echo "  ______ ______ ______ ______ ______ ______ ______ ______ ______ ______
  |______|______|______|______|______|______|______|______|______|______|
                                                                         "
 
-env_arg="$1"
-
 case "${env_arg,,}" in
   dev)
-    env="dev"
+    env="tripmonkey-dev"
+    target="0.development"
     ;;
   staging)
-    env="stage"
+    env="tripmonkey-stage"
+    target="1.staging"
     ;;
   prod)
-    env="prod"
+    env="tripmonkey-prod"
+    target="2.production"
     ;;
   *)
-    env="dev"
+    env="tripmonkey-dev"
+    target="0.development"
     ;;
 esac
 
+minikube start --profile tripmonkey
 minikube addons enable metrics-server
-minikube start
-./deploy-configs.sh $env
-./deploy-secrets.sh $env
-./install-dependencies.sh $env
+minikube addons enable ingress
+echo "Installing dependencies (Kafka and Mongo Operators) for environment: $env"
+kubectl apply -f https://strimzi.io/install/latest?namespace=default -n default
+kubectl apply -f build/mongodb-kubernetes-operator/config/crd/bases/mongodbcommunity.mongodb.com_mongodbcommunity.yaml
+kubectl apply -f build/mongodb-kubernetes-operator/deploy/clusterwide
+kubectl apply -k build/mongodb-kubernetes-operator/config/rbac/ --namespace default
+kubectl create -f build/mongodb-kubernetes-operator/config/manager/manager.yaml --namespace default
+kubectl apply -f k8s/environments/$target/pre-deploy
